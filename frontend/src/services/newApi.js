d09123661361b5dcfaf29fd71d7071cc49eec25a
Sathyan <sathyana3011@gmail.com>
2026-04-10 05:31:39 -0700

    Feat : Backend deploy and update base URL

--- frontend/src/services/newApi.js
+++ frontend/src/services/newApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://job-portal-fullstack-44qz.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
