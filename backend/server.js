11492cd513698bf158616bdf5b8db595e64ff203
Sathyan <sathyana3011@gmail.com>
2026-05-13 04:40:43 -0700

    Fix : CORS error

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,17 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:5173", "https://jobist.netlify.app"];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
